3fceeb8c040ea869c5cd602afb0ac51cfda984a0
Fred R <fred121@toothwit.ch>
2017-03-08 02:55:31 +0100

    Fixing layout

--- src/main/java/ch/toothwit/voidwarp/events/PlayerEventListener.java
+++ src/main/java/ch/toothwit/voidwarp/events/PlayerEventListener.java
@@ -11,13 +11,15 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import ch.toothwit.voidwarp.main.Settings; 
 
 public class PlayerEventListener implements Listener {
-	@EventHandler
-    public void onEntityDamage(EntityDamageEvent event) {
+	
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) { 
         if (event.getEntityType() == EntityType.PLAYER && event.getCause() == EntityDamageEvent.DamageCause.VOID) {
             Player p = (Player)event.getEntity(); 
             Location l = Settings.get().getWarpLocation(); 
             l.add(0.5, 0.5, 0.5); 
             p.teleport(l); 
         }
-    }
+    } 
+    
 }
